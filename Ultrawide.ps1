ff916d9975c54fdd344b8efa8d929ce77254247f
unknown <9328629+sharkusmanch@users.noreply.github.com>
2025-09-15 13:19:05 -0700

    Add frontend variable to ultrawide

--- Ultrawide.ps1
+++ Ultrawide.ps1
@@ -2,6 +2,8 @@ Import-Module "$PSScriptRoot\SharedLibrary.psm1"
 
 Set-RTSS-Frame-Limit -configFilePath "$env:USERPROFILE\scoop\persist\rtss\Profiles\Global" -newLimit 0
 
+$Frontend = Get-ConsoleFrontend
+
 & "$env:ProgramData\chocolatey\bin\MonitorSwitcher.exe" -load:"$env:APPDATA\MonitorSwitcher\Profiles\Ultrawide.xml"
 & "$env:ProgramFiles (x86)\Steam\steam.exe" -start steam://close/bigpicture
 
@@ -9,4 +11,7 @@ Start-Sleep 3
 & "$env:ProgramFiles\LGTV Companion\LGTV Companion.exe" -of -screenoff -sethdmi1 Device1 "Living Room"
 
 & "$env:USERPROFILE\scoop\shims\autohotkey.exe" .\desktop.ahk
-& "$env:LOCALAPPDATA\Playnite\Playnite.DesktopApp.exe"
+
+if ($Frontend -eq "Playnite") {
+    & "$env:LOCALAPPDATA\Playnite\Playnite.DesktopApp.exe"
+}
